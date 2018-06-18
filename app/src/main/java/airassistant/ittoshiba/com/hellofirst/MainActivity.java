ec9d799f5ed151153128147f09674e2cf2d31d74
liangbaochang <liangbcAndroid@163.com>
2018-06-18 18:31:17 +0800

    本地新添加的测试数据

---
+++ app/src/main/java/airassistant/ittoshiba/com/hellofirst/MainActivity.java
@@ -0,0 +1,19 @@
+package airassistant.ittoshiba.com.hellofirst;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("");
+        System.out.println("test1的分支");
+        System.out.println("test2分支的代码");
+        System.out.println("把两个分支进行合并");
+        
+        System.out.println("测试分支");
+    }
+}
