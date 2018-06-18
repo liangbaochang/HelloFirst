9fac18c6531e5f1d66ae860fe515cab78253a07e
liangbaochang <liangbcAndroid@163.com>
2018-06-18 21:10:55 +0800

    本地的 dev 分支做的修改

--- app/src/main/java/airassistant/ittoshiba/com/hellofirst/MainActivity.java
+++ app/src/main/java/airassistant/ittoshiba/com/hellofirst/MainActivity.java
@@ -13,33 +13,35 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("test1的分支");
         System.out.println("test2分支的代码");
         System.out.println("把两个分支进行合并");
-        
+
         System.out.println("测试分支");
         System.out.println("本地新添加的数据");
 
         System.out.println("第二个测试代码");
-        
+
         System.out.println("第三个测试代码");
 
         System.out.println("第四次测试代码");
-        
+
         System.out.println("服务端修改了代码");
 
         System.out.println("工作区修改与服务器冲突");
-        
+
         System.out.println("创建冲突的情形");
 
         System.out.println("在 studio 中修改代码");
-        
+
         System.out.println("在 远端 中修改代码");
 
         System.out.println("在本地修改代码制造冲突");
 
         System.out.println("在 远端 中修改代码制造冲突");
-        
+
         System.out.println("在 远端 中修改代码在本地直接拉去看看能不能覆盖本地代码");
 
         System.out.println("在本地修改代码");
+
+        System.out.println("本地的 dev 分支做的修改");
 
     }
 }
